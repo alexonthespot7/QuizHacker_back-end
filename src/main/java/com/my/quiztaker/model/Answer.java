a198d4e1619f221cf071c4c4f6a5e2e7142408f0
alexonthespot7 <bgw595@myy.haaga-helia.fi>
2023-12-08 13:43:28 +0200

    Test commit

--- src/main/java/com/my/quiztaker/model/Answer.java
+++ src/main/java/com/my/quiztaker/model/Answer.java
@@ -9,7 +9,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
+//test commit
 @Entity
 public class Answer {
 	@Id
