d37597b00aa926aed0ad6ff59bbacd54cb6f6bf6
alexonthespot7 <bgw595@myy.haaga-helia.fi>
2023-12-08 14:47:56 +0200

    Upgraded the depreceated methd

--- src/main/java/com/my/quiztaker/SecurityConfig.java
+++ src/main/java/com/my/quiztaker/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.my.quiztaker;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +10,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -23,9 +26,8 @@ import com.my.quiztaker.service.UserDetailServiceImpl;
 
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 	@Autowired
 	private UserDetailServiceImpl userDetailsService;
 	
@@ -35,52 +37,54 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private AuthEntryPoint exceptionHandler;
 	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().cors().and()
-		.sessionManagement()
-		.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-		.authorizeRequests()
-		.antMatchers(HttpMethod.GET, "/quizzes", "/quizzes/*", "/questions", "/questions/*", "/answers/*", "/difficulties", "/categories", "/users").permitAll()
-		.antMatchers(HttpMethod.POST, "/login", "/signup", "/verify/*", "/resetpassword").permitAll()
-		.antMatchers(HttpMethod.DELETE, "/deletequestion/*", "/deletequiz/*").authenticated()
-		.antMatchers(HttpMethod.POST, "/changepassword", "/updatequiz", "/savequestions/*", "/publishquiz/*", "/sendattempt/*", "/updateavatar/*").authenticated()
-		.antMatchers(HttpMethod.GET, "/createquiz", "/quizzesbyuser/*", "/usersauth/*", "/users/*", "/personalquizzes/*", "/getavatar/*").authenticated()
-		.anyRequest().hasAuthority("ADMIN").and()
-		.exceptionHandling()
-		.authenticationEntryPoint(exceptionHandler).and()
-		.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
-	}
-	
+	private static final String[] SWAGGER_PATHS = {"/swagger-ui/**", "/v3/api-docs/**", "/api-docs/**", "/v3/api-docs", "/swagger-ui.html"};
+
 	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		
-		CorsConfiguration config = new CorsConfiguration();
-		
-		config.setAllowedOrigins(Arrays.asList("*")); //config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-		config.setAllowedMethods(Arrays.asList("*"));
-		config.setAllowedHeaders(Arrays.asList("*"));
-		config.setAllowCredentials(false);
-		config.applyPermitDefaultValues();
-		
-		source.registerCorsConfiguration("/**", config);
-		
-		return source;
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf((csrf) -> csrf.disable()).cors(withDefaults())
+				.sessionManagement(
+						(sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+						.requestMatchers(SWAGGER_PATHS).permitAll()
+						.requestMatchers(HttpMethod.GET, "/quizzes", "/quizzes/*", "/questions", "/questions/*", "/answers/*", "/difficulties", "/categories", "/users").permitAll()
+						.requestMatchers(HttpMethod.POST, "/login", "/signup", "/verify/*", "/resetpassword").permitAll()
+						.requestMatchers(HttpMethod.DELETE, "/deletequestion/*", "/deletequiz/*").authenticated()
+						.requestMatchers(HttpMethod.GET, "/createquiz", "/quizzesbyuser/*", "/usersauth/*", "/users/*", "/personalquizzes/*", "/getavatar/*").authenticated()
+						.requestMatchers(HttpMethod.POST, "/changepassword", "/updatequiz", "/savequestions/*", "/publishquiz/*", "/sendattempt/*", "/updateavatar/*").authenticated()
+						.anyRequest().hasAuthority("ADMIN"))
+				.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+				.exceptionHandling((exceptionHandling) -> exceptionHandling.authenticationEntryPoint(exceptionHandler));
+
+		return http.build();
 	}
 	
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService)
-			.passwordEncoder(new BCryptPasswordEncoder());
+		auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
 	}
 	
 	@Bean
-	public AuthenticationManager getAuthenticationManager() 
-			throws Exception {
-		return authenticationManager();
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
 	}
 	
-		
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(Arrays.asList("*"));
+		config.setAllowedMethods(Arrays.asList("*"));
+		config.setAllowedHeaders(Arrays.asList("*"));
+		config.setAllowCredentials(false);
+		config.applyPermitDefaultValues();
+
+		source.registerCorsConfiguration("/**", config);
+		return source;
+	}		
 	
 }
