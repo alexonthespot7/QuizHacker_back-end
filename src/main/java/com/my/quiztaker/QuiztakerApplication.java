554db4b94b0c0ab53d70a20014aa0d37dd25ed73
alexonthespot7 <bgw595@myy.haaga-helia.fi>
2023-12-15 22:42:58 +0200

    Hard-coded records are commented

--- src/main/java/com/my/quiztaker/QuiztakerApplication.java
+++ src/main/java/com/my/quiztaker/QuiztakerApplication.java
@@ -19,29 +19,29 @@ public class QuiztakerApplication {
 		SpringApplication.run(QuiztakerApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner tournamentDemo(DifficultyRepository difficultyRepository,
-			CategoryRepository categoryRepository, UserRepository userRepository) {
-		return (args) -> {
-			Difficulty hardDifficulty = new Difficulty("Hard", 3);
-			Difficulty mediumDifficulty = new Difficulty("Medium", 2);
-			Difficulty smallDifficulty = new Difficulty("Easy", 1);
-			difficultyRepository.save(hardDifficulty);
-			difficultyRepository.save(mediumDifficulty);
-			difficultyRepository.save(smallDifficulty);
-			
-			Category otherCategory = new Category("Other");
-			Category itCategory = new Category("IT");
-			categoryRepository.save(otherCategory);
-			categoryRepository.save(itCategory);
-			
-			User user1 = new User("user1", "$2a$12$YV/TXHTDhWz5Y41p7X6WfegbFVF4/chhCzYtbXKm0gHxltazlbDoe", "USER", "user1@mail.com", null, true);
-			userRepository.save(user1);
-			
-			User user2 = new User("user2", "$2a$12$YV/TXHTDhWz5Y41p7X6WfegbFVF4/chhCzYtbXKm0gHxltazlbDoe", "USER", "user2@mail.com", null, true);
-			userRepository.save(user2);
-			
-		};
-	}
+//	@Bean
+//	public CommandLineRunner tournamentDemo(DifficultyRepository difficultyRepository,
+//			CategoryRepository categoryRepository, UserRepository userRepository) {
+//		return (args) -> {
+//			Difficulty hardDifficulty = new Difficulty("Hard", 3);
+//			Difficulty mediumDifficulty = new Difficulty("Medium", 2);
+//			Difficulty smallDifficulty = new Difficulty("Easy", 1);
+//			difficultyRepository.save(hardDifficulty);
+//			difficultyRepository.save(mediumDifficulty);
+//			difficultyRepository.save(smallDifficulty);
+//			
+//			Category otherCategory = new Category("Other");
+//			Category itCategory = new Category("IT");
+//			categoryRepository.save(otherCategory);
+//			categoryRepository.save(itCategory);
+//			
+//			User user1 = new User("user1", "$2a$12$YV/TXHTDhWz5Y41p7X6WfegbFVF4/chhCzYtbXKm0gHxltazlbDoe", "USER", "user1@mail.com", null, true);
+//			userRepository.save(user1);
+//			
+//			User user2 = new User("user2", "$2a$12$YV/TXHTDhWz5Y41p7X6WfegbFVF4/chhCzYtbXKm0gHxltazlbDoe", "USER", "user2@mail.com", null, true);
+//			userRepository.save(user2);
+//			
+//		};
+//	}
 
 }
